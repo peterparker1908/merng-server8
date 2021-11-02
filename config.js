98e9390d0e6df9386b734b97cc40029f023d9b02
peterparker1908 <phieulang1908@gmail.com>
2021-11-02 22:23:32 +0700

    init

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://ilovemycrush1908:EFjbA2NhDJXwrsfo@cluster0.etdi8.mongodb.net/mydb?retryWrites=true&w=majority',
+    SECRET_KEY: 'Some very secret key'
+}
